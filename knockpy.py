cfc79a44bc7662aaec5d42cb5ab05c94ce4c4d62
Madhukar Kumar <88223881+erx21@users.noreply.github.com>
2023-03-16 00:38:49 +0530

    Add files via upload

---
+++ knockpy.py
@@ -0,0 +1,76 @@
+from flask import Flask, request, jsonify
+import os
+import pymongo
+from bson import json_util
+import json
+import glob
+
+app = Flask(__name__)
+mongo_client = pymongo.MongoClient('mongodb+srv://vaibhav:vaib12345@vaibemsec.ioi6mqs.mongodb.net/')
+mongo_db = mongo_client['SubTest']
+
+@app.route('/')
+def home():
+    return "Welcome to the knockpy app!"
+
+@app.route('/search', methods=['POST'])
+def search():
+    req_json = request.json
+    domains = req_json['domains']
+    results = []
+
+    for domain in domains:
+        db_verify = mongo_db.knockpy.find_one({'domain': domain})
+
+        if db_verify is None:
+            knockpy_output = os.system(f'knockpy {domain}')
+            
+
+            directory_path = "/home/er_x/Downloads/demo/knockpy_report"
+            all_files = glob.glob(os.path.join(directory_path, "*"))
+            all_files.sort(key=os.path.getmtime, reverse=True)
+            latest_file = all_files[0]
+            os.rename(latest_file, os.path.join(directory_path, "knockpy_output.json"))
+
+
+            with open('knockpy_report/knockpy_output.json','r') as f:
+                subdomain= json.load(f)
+                # for line in f:
+                #     subdomains.append(line.strip())
+            # subdomains= json.loads(knockpy_output.json)
+            
+
+            # with open("knockpy_subdomains.txt", "w") as f:
+            #     f.write("\n".join(subdomains))
+
+            # temp_result= {}
+            # for key,value in subdomains :
+            #     ipaddr=value['ipaddr']
+            #     temp_result[key]=ipaddr
+
+
+            mongo_db.knockpy.insert_one({
+                'domain': domain,
+                'subdomains' :subdomain,
+                # 'output': output_data,
+            })
+
+        # Retrieve data from MongoDB
+        
+        domain_data = mongo_db.knockpy.find_one({'domain': domain})
+       
+        
+            
+        
+            
+
+        results.append(domain_data)
+       
+
+        clear_space = os.system('rm knockpy_report/knockpy_output.json')
+
+    json_results = json_util.dumps(results)
+    return jsonify(json_results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
